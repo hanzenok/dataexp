09daeb0678017892b84823bfe93ddc5895d34f2b
Mykhailo Ganza <hanzenok@gmail.com>
2016-05-04 15:16:08 +0200

    Moved the SidebarController to the SourceListController and it's separate file

---
+++ app/js/controllers/SourcesListController.js
@@ -0,0 +1,24 @@
+angular.module('MainApp')
+.controller('SourcesListController', function($scope, $mdDialog){
+
+	$scope.sources = [
+		{ name: 'Mongo', wanted: true },
+		{ name: 'SQL', wanted: true },
+		{ name: 'File', wanted: false }
+	];
+
+	$scope.showAlert = function(event){
+
+		$mdDialog.show( 
+
+			$mdDialog.alert()
+				.parent(angular.element(document.querySelector('#content')))
+				.clickOutsideToClose(true)
+				.title('Alert title')
+				.textContent('Alert text')
+				.ariaLabel('Alert Dialog')
+				.ok('OK!')
+				.targetEvent(event)
+		);
+	};
+});
