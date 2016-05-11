d33418cfab2ee92ee576f42b5f78f3864617b15b
Mykhailo Ganza <hanzenok@gmail.com>
2016-05-11 15:04:56 +0200

    Finally figured out how to delete chips in the dropped zone

--- app/js/controllers/DragCtrl.js
+++ app/js/controllers/DragCtrl.js
@@ -1,8 +1,35 @@
     angular.module('MainApp')
-        .service('draggables', function(){
+        /*.factory('draggables', function(){
             
-            var objects = [];
+            var draggables = [];
+            var draggablesService = {};
 
+            draggablesService.add = function(draggable){
+
+                console.log('add');
+                console.log(draggables);
+
+                var index = draggables.indexOf(draggable);
+                if (index == -1){
+
+                    draggables.push(draggable);
+                }
+            }
+
+            draggablesService.get = function(){
+
+                return draggables;
+            }
+
+            draggablesService.clear = function(){
+
+                console.log('clear func');
+                draggables = [];
+            }
+
+            return draggablesService;
+
+            //diff kind
             this.objects = objects;
 
             this.clear = function(){
@@ -16,19 +43,20 @@
                 this.objects = objects;
             }
 
-        })
-        .controller('DragCtrl1', function ($scope, draggables) {
-            
-            $scope.droppedObjects1 = draggables.objects;
+        })*/
+        .controller('DragCtrl1', function ($scope, $rootScope) {
 
+            // $scope.droppedObjects1 = draggables.get();
+            // $scope.onDropComplete1 = draggables.add;
+
+            $rootScope.droppedObjects1 = [];
             $scope.onDropComplete1=function(data,evt){
                 console.log('onDropComplete:');
                 console.log(data);
-                var index = draggables.objects.indexOf(data);
+                var index = $rootScope.droppedObjects1.indexOf(data);
                 if (index == -1){
 
-                    draggables.objects.push(data);
-                    $scope.droppedObjects1 = draggables.objects;
+                    $rootScope.droppedObjects1.push(data);
                 }
                 
 
@@ -36,19 +64,21 @@
 
       })
 
-        .controller('DragCtrl2', function($scope, draggables){
+        .controller('DragCtrl2', function($scope, $rootScope){
 
-        $scope.onDropComplete2=function(data,evt){
+            $scope.onDropComplete2 = function(data,evt){
 
-            console.log('onDropComplete2:');
-            //console.log(draggables.objects);
-            console.log(data);
-            draggables.clear();
+                console.log('onDropComplete2:');
+                //console.log(draggables.objects);
+                console.log(data);
+                console.log($rootScope.droppedObjects1);
+                $rootScope.droppedObjects1 = [];
+                //draggables.clear();
 
-            // var index = $scope.droppedObjects1.indexOf(data);
-            // if (index > -1) {
-            //     $scope.droppedObjects1.splice(index, 1);
-            // }
+                // var index = $scope.droppedObjects1.indexOf(data);
+                // if (index > -1) {
+                //     $scope.droppedObjects1.splice(index, 1);
+                // }
 
-        }
+            }
         });
