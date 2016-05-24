51434562c2f77392e8743641f530ed4eba1f7a16
Mykhailo Ganza <hanzenok@gmail.com>
2016-05-24 11:06:04 +0200

    Added the DOM's for the right side nav

---
+++ app/js/controllers/RightNavCtrl.js
@@ -0,0 +1,9 @@
+angular.module('MainApp')
+	.controller('RightNavCtrl', function($scope){
+
+		$scope.interp_type = 'linear';
+		$scope.reduc_type = 'skip';
+		$scope.from_date = '';
+		$scope.to_date = '';
+
+	})
