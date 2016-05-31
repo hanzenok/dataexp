88184e6fa0a75a8811a748d0bbb0fd00975d9fae
Mykhailo Ganza <hanzenok@gmail.com>
2016-05-31 11:04:23 +0200

    Added images to represent each chart

--- server/rest/GetTimeseries.js
+++ server/rest/GetTimeseries.js
@@ -65,9 +65,6 @@ TS.getTimeseries = function(req, res){
 		Promise.all(promises)
 		.then(function(datasets){
 
-			console.log('datasets');
-			console.log(datasets);
-
 			//callback
 			var callback = function(err, data){
 
@@ -104,8 +101,6 @@ TS.getTimeseries = function(req, res){
 
 				tsproc_config.timeseries.push(tmp);
 			});
-
-			console.log(JSON.stringify(tsproc_config));
 
 			//instantiate the timeseries processor
 			var tsp = new tsproc(datasets, tsproc_config, callback);
