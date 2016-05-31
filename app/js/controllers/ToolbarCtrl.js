179772ac28a2e550fabbdea6559c38227c6af93e
Mykhailo Ganza <hanzenok@gmail.com>
2016-05-31 15:56:32 +0200

    Bug fixed: now the chart images are no longer blinking

--- app/js/controllers/ToolbarCtrl.js
+++
@@ -1,9 +0,0 @@
-angular.module('MainApp')
-	.controller('ToolbarCtrl', function($scope, $rootScope, $mdSidenav){
-
-		var state = true;
-		$scope.toggle = function(){
-			
-			console.log('toogle sidenav');
-		}
-	})
