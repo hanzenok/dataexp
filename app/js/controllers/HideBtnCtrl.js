b09e755b4885b023060ccb221ccfd5a35890cb9f
Mykhailo Ganza <hanzenok@gmail.com>
2016-06-07 15:29:30 +0200

    Finished with the hide button

--- app/js/controllers/HideBtnCtrl.js
+++ app/js/controllers/HideBtnCtrl.js
@@ -2,8 +2,14 @@ angular.module('MainApp')
 	.controller('HideBtnCtrl', function ($scope, $rootScope) {
 
 	$rootScope.hideFooter = false;
+	$scope.image = 'images/down.svg';
 	$scope.toogle = function(){
 
+		//toogle the footer
 		$rootScope.hideFooter = !$rootScope.hideFooter;
+
+		//toogle the image
+		$scope.image = ($rootScope.hideFooter) ? 'images/up.svg' : 'images/down.svg';
 	}
+	
 });
