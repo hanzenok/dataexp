286174fb8907138cca904cabe10272e0ac688c77
Mykhailo Ganza <hanzenok@gmail.com>
2016-08-22 16:43:43 +0200

    Added a README file. Some doc corrections

---
+++ doc/api.js
@@ -0,0 +1,73 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "CanvasChartsService",
+        "ChartsCtrl",
+        "DCChartsService",
+        "DeleteSource",
+        "DeleteZoneCtrl",
+        "DialogCtrl",
+        "FabCtrl",
+        "FieldsService",
+        "FileConnector",
+        "GetFields",
+        "GetSources",
+        "GetStores",
+        "GetTimseries",
+        "HideBtnCtrl",
+        "LeftNavCtrl",
+        "LoaderCtrl",
+        "MongoConnector",
+        "MovableChartsCtrl",
+        "MysqlConnector",
+        "PutSource",
+        "RightNavCtrl",
+        "SourcesService",
+        "StoresService",
+        "TimeseriesService",
+        "ToolBarCtrl",
+        "main",
+        "routes",
+        "server"
+    ],
+    "modules": [
+        "Connectors",
+        "Controllers",
+        "RestApi",
+        "Services",
+        "client",
+        "server"
+    ],
+    "allModules": [
+        {
+            "displayName": "client",
+            "name": "client"
+        },
+        {
+            "displayName": "Connectors",
+            "name": "Connectors",
+            "description": "Connectors to different types of storages."
+        },
+        {
+            "displayName": "Controllers",
+            "name": "Controllers",
+            "description": "Angualr.js controllers."
+        },
+        {
+            "displayName": "RestApi",
+            "name": "RestApi",
+            "description": "Rest API's offered by server."
+        },
+        {
+            "displayName": "server",
+            "name": "server"
+        },
+        {
+            "displayName": "Services",
+            "name": "Services",
+            "description": "Angualr.js services."
+        }
+    ],
+    "elements": []
+} };
+});
